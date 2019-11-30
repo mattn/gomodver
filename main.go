7ef5cd4b6dce3ba2e8305146818cb6be8b172be7
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-11-30 13:37:37 +0900

    Add code

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "%s [mod]\n", os.Args[0])
+		os.Exit(1)
+	}
+	resp, err := http.Get(fmt.Sprintf("https://proxy.golang.org/%s/@v/list", os.Args[1]))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %s\n", os.Args[0], err.Error())
+		os.Exit(1)
+	}
+	io.Copy(os.Stdout, resp.Body)
+}
